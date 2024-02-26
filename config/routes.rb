1db2d855ca9cfd18f49643730486fcec4d8c97f7
ke-me <fukudakei@fukudatkonpyuta.lan>
2024-02-27 00:02:01 +0900

    ログインページのmvc作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/new'
+  get 'user/new'
   resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "posts#index"
@@ -6,7 +8,7 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
+  get "signup" => "users#new"
   # Defines the root path route ("/")
   # root "posts#index"
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_25_061145) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_26_143806) do
   create_table "posts", force: :cascade do |t|
     t.string "title", null: false
     t.text "content", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
