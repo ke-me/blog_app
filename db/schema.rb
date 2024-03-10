3fd082b2b8c1c42d8bed25711dd275a9eca52fd2
ke-me <fukudakei@fukudatkonpyuta.lan>
2024-03-10 12:45:08 +0900

    userモデルのベースを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_27_140657) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_05_135507) do
   create_table "posts", force: :cascade do |t|
     t.string "title", null: false
     t.text "content", null: false
@@ -23,6 +23,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_27_140657) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
