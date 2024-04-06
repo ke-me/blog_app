6d02b33a6c6b144753f9621011a6e9063e37899f
ke-me <fukudakei@fukudatkonpyuta.lan>
2024-04-06 16:51:06 +0900

    ログイン中かどうかを判定→レイアウト条件分岐

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,13 +1,20 @@
 module SessionsHelper
     # log_inというヘルパーメソッドを定義
+    # 渡されたユーザーでログインする
     def log_in(user)
-        # 渡されたユーザーでログインする
         session[:user_id] = user.id
     end
 
+    # find_byでユーザーを呼び出す（データベースから探す？）
+    # @current_userはインスタンス変数。一度呼び出したら、再度呼び出さないでいいように@current_userに代入している
     def current_user
         if session[:user_id]
             @current_user ||= User.find_by(id: session[:user_id])
         end
+    end
+
+    # current_userがnillでない。つまり、ログイン中かどうかを定義するメソッド
+    def logged_in?
+        !current_user.nil?
     end
 end
